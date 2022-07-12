112db1b631c3a1ce37e7ef1ccc6bead4f4664f73
voyager <cdspacenoob@gmail.com>
2022-07-12 19:08:13 +0900

    220712 Java_Basecode

---
+++ Today_Java/Base02_for.java
@@ -0,0 +1,41 @@
+package lesson.day02;
+
+public class Base02_for {
+
+	public static void main(String[] args) {
+//		int number = 2398;
+//		int result = 0;
+//		int total = 0;
+//		
+//		// 숫자의 자릿수가 반복횟수가 된다
+//		result = number % 10;
+//		total += result;
+//		System.out.println(result);
+//		number = number / 10;
+//
+//		result = number % 10;
+//		total += result;
+//		System.out.println(result);
+//		number = number / 10;
+//		
+//		result = number % 10;
+//		total += result;
+//		System.out.println(result);
+//		number = number / 10;
+//		
+//		result = number % 10;
+//		total += result;
+//		System.out.println(result);
+//		number = number / 10;
+		
+		
+        /* 이 부분을 자동화하는 것이 목표 		
+        System.out.println(number%10);
+		System.out.println((number%100)/10);
+		System.out.println((number%1000)/100);
+		System.out.println(number/1000);
+		*/
+
+	}
+
+}
